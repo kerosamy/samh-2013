87eee8e9f96609f43fe7be330ff4a6beae508ab0
kero <kerosamy42004@gmail.com>
2023-12-14 23:22:10 +0200

    level 2.1

--- start.c
+++ start.c
@@ -7,7 +7,29 @@
 #define blue  "\033[1;34m"
 #define white  "\033[1;37m"
 #define rest  "\033[1;0m"
-
+int select2(int i){
+   int j ;
+printf("\n///////////////////////////\n");
+printf("///////////////////////////\n");
+printf(yellow"please select the mode <3\n"rest);
+printf(green"1-human Vs human \n"rest blue"2-human Vs computer\n"rest);
+printf(white"your mode will be :"rest);
+scanf("%i",&j);
+switch (j)
+{
+case 1:
+   
+   break;
+case 2:
+   
+   break;
+default:
+printf(red"sorry we can not understand you please try agin"rest);
+select2(i);
+   break;
+}
+return 0 ;
+}
 
 int select1(){
  int i;
@@ -17,15 +39,16 @@ printf("\n///////////////////////////////////////\n");
 printf("///////////////////////////////////////\n");
 printf(yellow"please select the leve <3\n"rest);
 printf(green"1-beginner 2x2 \n"rest red"2-expert 5x5\n"rest white"your game will be :"rest);
-scanf("%li",&i);
+scanf("%i",&i);
 
 switch (i)
 {
 case 1 :
    
+   select2(i);
    break;
 case 2 :
-   
+   select2(i);
    break;
 default:
   
